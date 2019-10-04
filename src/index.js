b57317e6577636b1a01e8f34c328b6b8e5e4a734
Zach Preston <zpdrums@comcast.net>
2019-10-03 22:23:35 -0400

    remove preloader scene class and update game class to call start scene

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import Phaser from 'phaser';
 import config from './config/config';
 import StartScene from './scenes/StartScene';
 import GameOverScene from './scenes/GameOverScene';
-import PreloaderScene from './scenes/PreloaderScene';
 import LevelOneScene from './scenes/LevelOneScene';
 
 class Game extends Phaser.Game {
 	constructor() {
 		super(config);
-		this.scene.add('Preloader', PreloaderScene);
 		this.scene.add('Start', StartScene);
 		this.scene.add('Level 01', LevelOneScene);
 		this.scene.add('End', GameOverScene);
 
-		this.scene.start('Preloader');
+		this.scene.start('Start');
 	}
 }
 
