597377e9ebbfdc176cb8c1df90e0f63f8aea9a6e
Zach Preston <zpdrums@comcast.net>
2019-10-03 22:12:38 -0400

    add player and keyboard logic to level 1 scene class

--- src/scenes/LevelOneScene.js
+++ src/scenes/LevelOneScene.js
@@ -1,11 +1,46 @@
 import Phaser from 'phaser';
+import config from '../config/config';
+import iconSpritesheet from '../assets/images/icon0.png';
 
 export default class Level01 extends Phaser.Scene {
 	constructor() {
 		super('Level 01');
 	}
 
+	preload() {
+        this.load.spritesheet('icons', iconSpritesheet, { frameWidth: 16, frameHeight: 16 }, 71);
+	}
+
 	create() {
 		this.cameras.main.setBackgroundColor('#2E8B57'); // seagreen
+
+		this.player = this.physics.add.sprite(config.width / 2, config.height / 2, 'icons', 21);
+		this.player.setScale(2);
+        this.player.speed = 4;
+		this.player.setCollideWorldBounds(true);
+
+		this.cursorKeys = this.input.keyboard.createCursorKeys();
+	}
+
+	update() {
+		this.player.setVelocity(0);
+
+		if (this.cursorKeys.left.isDown)
+		{
+		    this.player.setVelocityX(-300);
+		}
+		else if (this.cursorKeys.right.isDown)
+		{
+		    this.player.setVelocityX(300);
+		}
+
+		if (this.cursorKeys.up.isDown)
+		{
+		    this.player.setVelocityY(-300);
+		}
+		else if (this.cursorKeys.down.isDown)
+		{
+		    this.player.setVelocityY(300);
+		}
 	}
 }
