190e73e2891d0b18b8b313aac510d067400196cc
Zach Preston <zpdrums@comcast.net>
2019-10-27 15:05:22 -0400

    update main config file to fit game on entire viewport

--- src/config/game.js
+++ src/config/game.js
@@ -1,14 +1,13 @@
 export default {
 	type: Phaser.AUTO,
 	parent: 'phaser-example',
-	width: 800,
-	height: 600,
+	width: window.innerWidth,
+	height: window.innerHeight,
 	pixelArt: true,
 	physics: {
 		default: 'arcade'
 	},
 	scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        mode: Phaser.Scale.FIT
     }
 };
