cbf493b8960ac2f92cda90943b737d18bfb925af
Zach Preston <zpdrums@comcast.net>
2019-10-28 21:47:25 -0400

    rename timer property in time label class

--- src/labels/TimeLabel.js
+++ src/labels/TimeLabel.js
@@ -3,18 +3,18 @@ import Phaser from 'phaser';
 class TimeLabel extends Phaser.GameObjects.Text {
 	constructor(config) {
 		super(config.scene, config.x, config.y, config.text, config.style);
-		this.timer = 2000;
-		this.setLabel(this.timer);
+		this.time = 2000;
+		this.setLabel(this.time);
 		config.scene.add.existing(this);
 	}
 
 	decrementTime() {
-		this.timer--;
-		this.setLabel(this.timer);
+		this.time--;
+		this.setLabel(this.time);
 	}
 
-	setLabel(timer) {
-		this.setText(`Time: ${Math.round(timer / 100)}`);
+	setLabel(time) {
+		this.setText(`Time: ${Math.round(time / 100)}`);
 	}
 }
 
