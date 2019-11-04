eab3346702eb9e071dd13a005055a4d6c98e6d55
Zach Preston <zpdrums@comcast.net>
2019-11-03 20:16:44 -0500

    increase explosion scale

--- src/sprites/Explosion.js
+++ src/sprites/Explosion.js
@@ -3,7 +3,7 @@ import Phaser from 'phaser';
 class Explosion extends Phaser.Physics.Arcade.Sprite {
 	constructor(config) {
 		super(config.scene, config.x, config.y, 'explosion');
-		this.setScale(2);
+		this.setScale(3);
 		config.scene.add.existing(this);
 		this.play('explode');
 	}
