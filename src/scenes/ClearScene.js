9b634f2ca69b6b3f3113a5f2b36c472e54236f92
Zach Preston <zpdrums@comcast.net>
2019-11-09 19:32:51 -0500

    add high score functionality

--- src/scenes/ClearScene.js
+++ src/scenes/ClearScene.js
@@ -9,6 +9,7 @@ class ClearScene extends Phaser.Scene {
 
     init(data) {
         this.score = data.score;
+        this.highScore = data.highScore;
         this.levels = data.levels;
         this.nextLevel = data.nextLevel;
     }
@@ -21,11 +22,13 @@ class ClearScene extends Phaser.Scene {
     create() {
         this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'clear-image');
         this.scoreText = this.add.text(this.cameras.main.width / 2, (this.cameras.main.height / 2) + 70, `SCORE: ${this.score}`, { fontSize: '32px', fill: '#fff' });
+        this.highScoreText = this.add.text(this.cameras.main.width / 2, (this.cameras.main.height / 2) + 100, `HIGH SCORE: ${this.highScore}`, { fontSize: '32px', fill: '#fff' });
         this.scoreText.setOrigin(0.5);
+        this.highScoreText.setOrigin(0.5);
         this.sound.add('clear-sound').play();
 
         if (this.nextLevel) {
-            this.nextLevelText = this.add.text(this.cameras.main.width / 2, (this.cameras.main.height / 2) + 120, 'Go to the next level', { fontSize: '32px', fill: '#fff' });
+            this.nextLevelText = this.add.text(this.cameras.main.width / 2, (this.cameras.main.height / 2) + 140, 'Go to the next level', { fontSize: '32px', fill: '#fff' });
             this.nextLevelText.setOrigin(0.5);
             this.input.on('pointerdown', () => {
                 this.scene.start('Gameplay', { levels: this.levels, level: this.nextLevel });
