c67e5233b6708675f658912d6029ba7322b1d147
Zach Preston <zpdrums@comcast.net>
2019-11-24 22:29:45 -0500

    add background music and move sounds to new audio folder

---
+++ src/assets/audio/index.js
@@ -0,0 +1,5 @@
+export { default as BombSound } from './sounds/bomb1.wav';
+export { default as ClearSound } from './sounds/se6.wav';
+export { default as FruitSound } from './sounds/se2.wav';
+export { default as GameOverSound } from './sounds/se7.wav';
+export { default as BackgroundMusic } from './music/jungle.mp3';
--- src/assets/sounds/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BombSound } from './bomb1.wav';
-export { default as ClearSound } from './se6.wav';
-export { default as FruitSound } from './se2.wav';
-export { default as GameOverSound } from './se7.wav';
