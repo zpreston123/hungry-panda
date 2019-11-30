99387ee97693e5f4bc8d5252cc49933fe2c720f6
Zach Preston <zpdrums@comcast.net>
2019-11-29 22:37:10 -0500

    add photo text to credits scene class

--- src/scenes/CreditsScene.js
+++ src/scenes/CreditsScene.js
@@ -18,7 +18,7 @@ class CreditsScene extends Phaser.Scene {
 		this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
 		this.madeByText = this.add.text(0, 0, 'Created By: Zach Preston', { fontSize: '26px', fill: '#fff' });
 		this.menuButton = new Button(this, 400, 500, 'blueButton1', 'blueButton2', 'Menu', 'Title');
-
+		this.photoText = this.add.text(0, 0, 'Photo by Yoal Desurmont on Unsplash', { fontSize: '20px', fill: '#fff' });
 		this.zone = this.add.zone(config.width / 2, config.height / 2, config.width, config.height);
 
 		Phaser.Display.Align.In.Center(
@@ -42,8 +42,12 @@ class CreditsScene extends Phaser.Scene {
 			110
 		);
 
-		this.madeByText.setY(1100);
-		this.menuButton.setY(1250);
+		Phaser.Display.Align.In.BottomRight(
+			this.photoText,
+			this.zone,
+			0,
+			-10
+		);
 	}
 }
 
