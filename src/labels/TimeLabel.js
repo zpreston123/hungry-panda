1099e86b5ffdf3082132d26fe84f9c90f8043cbd
Zach Preston <zpdrums@comcast.net>
2024-07-08 21:20:45 -0400

    reformat TimeLabel class

--- src/labels/TimeLabel.js
+++ src/labels/TimeLabel.js
@@ -3,9 +3,9 @@ import 'phaser';
 const formatTime = (time) => `Time: ${time}`;
 
 class TimeLabel extends Phaser.GameObjects.Text {
-	constructor(scene, x, y, time, style) {
-		super(scene, x, y, formatTime(time), style);
-	}
+    constructor(scene, x, y, time, style) {
+        super(scene, x, y, formatTime(time), style);
+    }
 }
 
 export default TimeLabel;
