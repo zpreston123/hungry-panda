f06d4e386f4f879b8649cdaee3dfe7421a996aa8
Zach Preston <zpdrums@comcast.net>
2024-07-08 21:20:33 -0400

    update ScoreLabel class: 1.  remove updateScoreText method 2.  transfer logic from above method to setScore method

--- src/labels/ScoreLabel.js
+++ src/labels/ScoreLabel.js
@@ -3,31 +3,26 @@ import 'phaser';
 const formatScore = (score) => `Score: ${score}`;
 
 class ScoreLabel extends Phaser.GameObjects.Text {
-	constructor(scene, x, y, score, style) {
-		super(scene, x, y, formatScore(score), style);
+    constructor(scene, x, y, score, style) {
+        super(scene, x, y, formatScore(score), style);
+        this.score = score;
+    }
 
-		this.score = score;
-	}
+    setScore(score) {
+        this.score = score;
+        this.setText(formatScore(this.score));
+    }
 
-	setScore(score) {
-		this.score = score;
-		this.updateScoreText();
-	}
-
-	add(points) {
-		this.setScore(this.score + points);
+    add(points) {
+        this.setScore(this.score + points);
     }
 
     subtract(points) {
-    	this.setScore(this.score - points);
+        this.setScore(this.score - points);
     }
 
     reset() {
-    	this.setScore(0);
-    }
-
-    updateScoreText() {
-		this.setText(formatScore(this.score));
+        this.setScore(0);
     }
 }
 
